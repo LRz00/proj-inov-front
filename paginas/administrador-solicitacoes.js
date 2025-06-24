a252016f4e74be289bb350047b7c5c29298bd0b4
Hiane <hianecfernandes@gmail.com>
2025-06-24 17:46:39 -0300

    tela de solicitacoes adm atualizada

---
+++ paginas/administrador-solicitacoes.js
@@ -0,0 +1,6 @@
+// Esse código ainda está vazio.
+// Será usado futuramente para buscar os dados reais do back-end.
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Tela de Solicitações do Administrador carregada.");
+});
